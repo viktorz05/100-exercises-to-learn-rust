1aee63cabb9d51b258c0fffaf5e2238705f36bea
Viktor Zepeda sanchez <viktor_zepeda_sanchez@sfu.ca>
2025-11-29 02:01:31 -0800

    so tired kms (01_enum)

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,20 +8,73 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
-#[derive(PartialEq, Debug)]
+#[derive( Debug, Clone, Copy)]
 pub struct SaturatingU16 {
     value: u16
 }
 impl From<u16> for SaturatingU16 {
     fn from(val: u16) -> Self {
         SaturatingU16 { value: val } 
-        
     }
 }
 
+impl From<u8> for SaturatingU16 {
+    fn from(val: u8) -> Self {
+        SaturatingU16 { value: val.into() } 
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(val: &u16) -> Self {
+        SaturatingU16 { value: (*val).into()} 
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(val: &u8) -> Self {
+        SaturatingU16 { value: (*val).into() } 
+    }
+}
+
+
 impl std::ops::Add for SaturatingU16 {
-    type Output = Self;
+    type Output = SaturatingU16;
     fn add(self, rhs: Self) -> Self::Output {
-        todo!()
+        self + rhs.value
+    }
+}
+
+impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        self + *rhs
+    }
+}
+
+impl std::ops::Add<&u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+    fn add(self, rhs: &u16) -> Self::Output {
+        let sum = self.value.saturating_add(*rhs);
+        Self {value: sum}
+    }
+}
+
+impl std::ops::Add<u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+    fn add(self, rhs: u16) -> Self::Output {
+        let sum = self.value.saturating_add(rhs);
+        Self {value: sum}
+    }
+}
+
+impl PartialEq for SaturatingU16 {
+    fn eq(&self, other : &SaturatingU16) -> bool {
+        self.value == other.value
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other : &u16) -> bool {
+        self.value == *other
     }
 }
