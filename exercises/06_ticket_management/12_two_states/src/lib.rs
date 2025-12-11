2fca0e4767e0ffdcab96addd7c1f867a718acd28
Viktor Zepeda sanchez <viktor_zepeda_sanchez@sfu.ca>
2025-12-11 03:14:03 -0800

    12_two_states

--- exercises/06_ticket_management/12_two_states/src/lib.rs
+++ exercises/06_ticket_management/12_two_states/src/lib.rs
@@ -11,6 +11,7 @@ use ticket_fields::{TicketDescription, TicketTitle};
 #[derive(Clone)]
 pub struct TicketStore {
     tickets: Vec<Ticket>,
+    count: u64
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -41,12 +42,27 @@ impl TicketStore {
     pub fn new() -> Self {
         Self {
             tickets: Vec::new(),
+            count : 0
         }
     }
 
-    pub fn add_ticket(&mut self, ticket: Ticket) {
-        self.tickets.push(ticket);
+    pub fn add_ticket(&mut self, draft: TicketDraft) -> TicketId {
+        let id = self.count;
+        self.count += 1;
+        let t = Ticket {
+            id: TicketId(id),
+            title: draft.title,
+            description: draft.description,
+            status: Status::ToDo,
+        };
+        self.tickets.push(t);
+        TicketId(id)
     }
+
+    pub fn get(&self, id: TicketId) -> Option<&Ticket> {
+        self.tickets.iter().find(|ticket| ticket.id == id)
+    }
+
 }
 
 #[cfg(test)]
