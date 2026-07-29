d38e857f60176f7c2af047ef38688c4281f40a01
Viktor Zepeda <vza12@sfu.ca>
2026-07-28 19:46:16 -0700

    almost done

--- exercises/08_futures/05_blocking/src/lib.rs
+++ exercises/08_futures/05_blocking/src/lib.rs
@@ -3,16 +3,20 @@
 //  deadlock between the caller and the server.
 //  Use `spawn_blocking` inside `echo` to resolve the issue.
 use std::io::{Read, Write};
+use std::net::TcpStream;
 use tokio::net::TcpListener;
-
+use tokio::task::spawn_blocking;
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
     loop {
         let (socket, _) = listener.accept().await?;
         let mut socket = socket.into_std()?;
-        socket.set_nonblocking(false)?;
-        let mut buffer = Vec::new();
-        socket.read_to_end(&mut buffer)?;
-        socket.write_all(&buffer)?;
+        spawn_blocking(move || -> Result<(), anyhow::Error> {
+            let mut buffer = Vec::new();
+            socket.set_nonblocking(false)?;
+            socket.read_to_end(&mut buffer)?;
+            socket.write_all(&buffer)?;
+            Ok(())
+        });
     }
 }
 
